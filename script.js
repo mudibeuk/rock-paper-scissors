f3ec78b14f2bda79268b4679380d2e517f84b2a1
mudibeuk <mudi.li@hotmail.com>
2026-09-25 14:57:36 +0100

    Add UI

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
 const beats = {
     rock: "scissors",
     paper: "rock",
     scissors: "paper",
 };
 
-playGame();
-
-function playGame() {
-    let rounds = 0;
-    while (rounds < 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-        rounds++;
-    }
-
-    if (humanScore === computerScore) {
-        console.log("It's a tie");
-    } else if (humanScore > computerScore) {
-        console.log("You win");
-    } else {
-        console.log("You lost");
-    }
-}
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    const validChoices = ["rock", "paper", "scissors"];
-    let choice;
+function playRound(event) {
 
-    do {
-        choice = prompt("What is your choice? rock, paper or scissors")?.toLowerCase();
-    } while (!validChoices.includes(choice));
+    let humanChoice = event.target.className;
+    let computerChoice = getComputerChoice();
 
-    return choice;
-}
+    let result = document.querySelector(".result");
+    let score = document.querySelector(".score");
 
-function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("It's a tie.");
+        result.textContent = "It's a tie.";
     } else if (beats[humanChoice] === computerChoice) {
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        result.textContent =`You win! ${humanChoice} beats ${computerChoice}.`;
     } else {
         computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+
+    if(humanScore < 5 && computerScore < 5){
+        score.textContent = `You have ${humanScore} points and the computer has ${computerScore} points.`;
+
+    }else{
+        score.textContent = humanScore> computerScore? "You reached 5 points and win!" : "the computer reached 5 points and win!";
     }
 }
